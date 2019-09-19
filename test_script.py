74be8e92385ce9cc517f3f9581b37bc09ef7bcad
jverecken <45732366+jverecken@users.noreply.github.com>
2019-09-19 01:01:19 -0400

    Faster + Cleaner Task 2
    moved class methods
    suppressed slow progress bar
    convergence mode

--- test_script.py
+++ test_script.py
@@ -8,9 +8,9 @@ from matplotlib import pyplot as plt
 
 clean_redwine = pd.read_csv('winequality/clean_redwine.csv',index_col=0)
 
-(X_train,y_train,X_val,y_val) = utils.preprocessing(clean_redwine)
+(X_train,y_train,X_val,y_val) = utils.preprocessing(clean_redwine,prop=0.8)
 
-mymodel = model.logistic_regression(X_train.shape[1])
+mymodel = model.logistic_regression(X_train.shape[1], num_epoch = 300, alpha_init = 0.1, threshold = 1e-6)
 metrics = mymodel.fit(X_train,y_train,X_val,y_val)
 
 # plot metrics
